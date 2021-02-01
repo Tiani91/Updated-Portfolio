21c47cb12e59952b8017ba7fcfb90d862ed205b2
Tiani Pierce <tianifball@gmail.com>
2021-02-01 16:05:28 -0600

    Update directory.js

--- js/directory.js
+++ js/directory.js
@@ -2,4 +2,4 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 ctx.font = '50px serif';
-ctx.strokeText('Hello world', 50, 90, 140);
+ctx.strokeText('Hello world', 0, 0, 0);
