1bec992f51eac4013281feb94768eb6e1ada1945
Tiani Pierce <tianifball@gmail.com>
2021-02-01 15:35:50 -0600

    updates

--- js/directory.js
+++ js/directory.js
@@ -9,7 +9,7 @@ gradient.addColorStop("0.5", "yellow");
 gradient.addColorStop("1.0", "blue");
 //fill with gradient
 ctx.strokeStyle = gradient;
-ctx.strokeText('Tianis Web Development', 5, 10, maxWidth);
+ctx.strokeText('Tianis Web Development', 50, 50, maxWidth);
 //Navigation
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
