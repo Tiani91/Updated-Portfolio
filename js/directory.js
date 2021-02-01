85b42d19fb241cdc286af0a3f0470783a86b5d8c
Tiani Pierce <tianifball@gmail.com>
2021-02-01 13:32:30 -0600

    Update directory.js

--- js/directory.js
+++ js/directory.js
@@ -2,7 +2,7 @@ var c = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 ctx.font = "38px Chakra Petch";
 //gradient
-var gradient = ctx.createLinearGradient(0,0,c.width,0);
+var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
 gradient.addColorStop("0", "green");
 gradient.addColorStop("0.5", "yellow");
 gradient.addColorStop("1.0", "blue");
