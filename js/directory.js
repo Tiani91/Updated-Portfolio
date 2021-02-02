7b80595b66f5a653a348be9caea3c4b0be6bc123
Tiani Pierce <tianifball@gmail.com>
2021-02-02 16:14:03 -0600

    piechart

--- js/directory.js
+++ js/directory.js
@@ -1,1 +1,23 @@
 $(document).ready(function(){});
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+// Draw the chart and set the chart values
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+  ['Skill', 'Percentage'],
+  ['Frontend Web Development', 90],
+  ['HTML/CSS', 70],
+  ['Javascript/jQuery', 50],
+  ['Sass and Bootstrap', 40],
+  ['React.js & Adobe Ill', 10]
+]);
+
+  // Optional; add a title and set the width and height of the chart
+  var options = {'width':550, 'height':400};
+
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+}
